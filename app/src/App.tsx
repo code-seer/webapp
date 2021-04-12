2ff0ffe23b079d153707e56c60ce66b7dfccfb76
Biz Melesse <bizmelesse@gmail.com>
2021-04-12 18:31:49 -0500

    Create NavBar and set tsconfig values

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,29 @@
 
-import React, {Component} from 'react';
+import * as React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarInstance from "./components/NavbarInstance";
 
-class App extends Component {
-  render() {
+class App extends React.Component<any, any> {
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.state = {
+            activeKey: null
+        };
+    }
+    handleSelect(eventKey: any) {
+        this.setState({
+            activeKey: eventKey
+        });
+    }
+
+    render() {
+      const { activeKey } = this.state;
+
     return (
-        <div>
-          <p>Hello, world!</p>
+        <div className="nav-wrapper">
+            <NavBarInstance activeKey={activeKey} onSelect={this.handleSelect} />
         </div>
     );
   }

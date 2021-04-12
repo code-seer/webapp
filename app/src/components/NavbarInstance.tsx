85676eaf8e4d7cf8f598e543d599f5f92fe6802b
Biz Melesse <bizmelesse@gmail.com>
2021-04-12 18:49:04 -0500

    Add center-aligned slogan

--- app/src/components/NavbarInstance.tsx
+++ app/src/components/NavbarInstance.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import * as Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
@@ -16,16 +15,20 @@ class NavBarInstance extends React.Component<NavBarInstanceProps> {
     render() {
         return (
             <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-light">Search</Button>
-                </Form>
+                <Navbar.Brand href="#home">LearNet</Navbar.Brand>
+                {/*<Nav className="mr-auto">*/}
+                {/*    <Nav.Link href="#home">Home</Nav.Link>*/}
+                {/*    <Nav.Link href="#features">Features</Nav.Link>*/}
+                {/*    <Nav.Link href="#pricing">Pricing</Nav.Link>*/}
+                {/*</Nav>*/}
+
+                <Container>
+                    <p className="Navbar-text-center mt-4 mb-4">Code Visualizer</p>
+                </Container>
+                {/*<Form inline>*/}
+                {/*    <FormControl type="text" placeholder="Search" className="mr-sm-2" />*/}
+                {/*    <Button variant="outline-light">Search</Button>*/}
+                {/*</Form>*/}
             </Navbar>
         );
     }
